48cf2cfdfe0da865dde5ae3d39d2d0a7a48040fe
j1xaro <konecnik.jan@gmail.com>
2020-05-07 19:44:22 +0200

    Update
    Dodal stran z vprasanji

---
+++ izbris.js
@@ -0,0 +1,16 @@
+function izbris()
+{
+    Swal.fire(
+        'Uspešno',
+        'Izbrisali ste vprašanje!',
+        'success'
+      )
+}
+function izbris2()
+{
+    Swal.fire(
+        'Uspešno',
+        'Izbrisali ste rezultat!',
+        'success'
+      )
+}
